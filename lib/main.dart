74b02fb022b7b1e61afbe2712cb344df31890921
Gassem-ilhem <74540458+Gassem-ilhem@users.noreply.github.com>
2023-07-24 13:16:50 +0100

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/app/modules/authentification/views/authentification_view.dart';
 
 import 'package:get/get.dart';
 
@@ -8,8 +9,9 @@ void main() {
   runApp(
     GetMaterialApp(
       title: "Application",
-      initialRoute: AppPages.INITIAL,
+      //initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      home: AuthentificationView(),
     ),
   );
 }
