e6d3a6f02d5de56c7107c7c94cc167aa379153e9
iheb meftah <ihebmeftah666@gmail.com>
2023-07-31 12:55:42 +0100

    remove appbar

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -10,54 +10,50 @@ class SplashView extends GetView<SplashController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(''),
-          centerTitle: true,
-        ),
         body: Column(
-          children: [
-            const SizedBox(height: 80),
-            Center(
-              child: Container(
-                  width: 150.0,
-                  height: 150.0,
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(217, 150, 50, 9),
-                    shape: BoxShape.circle,
-                  ),
-                  child: const Center(
-                      child: Text(
-                    "Logo",
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),
-                  ))),
-            ),
-            const SizedBox(height: 50),
-            const Center(
-              child: Text(
-                'Bienvenue .........',
-                style: TextStyle(
-                    fontSize: 22,
-                    fontFamily: 'Courgette',
-                    fontWeight: FontWeight.bold),
+      children: [
+        const SizedBox(height: 80),
+        Center(
+          child: Container(
+              width: 150.0,
+              height: 150.0,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(217, 150, 50, 9),
+                shape: BoxShape.circle,
               ),
-            ),
-            const SizedBox(height: 15),
-            const Center(
-              child: Text(
-                "Libérez l'expérience de réservation l'hôtel ultime qui améliore vos vacances",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: "Courgette",
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(height: 50),
-            const SpinKitCircle(
-              size: 100,
-              color: Color.fromRGBO(217, 150, 50, 9),
-            ),
-          ],
-        ));
+              child: const Center(
+                  child: Text(
+                "Logo",
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ))),
+        ),
+        const SizedBox(height: 50),
+        const Center(
+          child: Text(
+            'Bienvenue .........',
+            style: TextStyle(
+                fontSize: 22,
+                fontFamily: 'Courgette',
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(height: 15),
+        const Center(
+          child: Text(
+            "Libérez l'expérience de réservation l'hôtel ultime qui améliore vos vacances",
+            style: TextStyle(
+                fontSize: 20,
+                fontFamily: "Courgette",
+                fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        const SizedBox(height: 50),
+        const SpinKitCircle(
+          size: 100,
+          color: Color.fromRGBO(217, 150, 50, 9),
+        ),
+      ],
+    ));
   }
 }
